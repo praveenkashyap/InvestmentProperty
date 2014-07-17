08bd1cf0c3f4dec8234e866c58830e67dcee852e
Praveen Kashyap <praveenkashyap@gmail.com>
2014-07-17 10:09:27 -0700

    Added Tabs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 /**
  * @author Praveen
  */
+//To display the three tabs and hide them when focus is removed
+function tab(tab) {
+	document.getElementById('tab1').style.display = 'none';
+	document.getElementById('tab2').style.display = 'none';
+	document.getElementById('tab3').style.display = 'none';
+	document.getElementById('li_tab1').setAttribute("class", "");
+	document.getElementById('li_tab2').setAttribute("class", "");
+	document.getElementById('li_tab3').setAttribute("class", "");
+	document.getElementById(tab).style.display = 'block';
+	document.getElementById('li_'+tab).setAttribute("class", "active");
+}
 
 /*function printData(globalData, yearlyChanges, expenses){
 	var str = "local Storage: " + localStorage.length;
